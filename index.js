20dbf4a22307a8ba4fbe5dd13a2b12e7839eaeca
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 14:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 const obj = { prop: 1 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  object[key] = value
   return object
 }
